c026598b33399eb0b00e2001be523611168b76cc
jdaniel166 <jesudossdaniel@gmail.com>
2025-07-07 23:44:50 -0400

    Update suffixArray.py

--- algorithms/suffixArray.py
+++ algorithms/suffixArray.py
@@ -5,16 +5,16 @@ import re
 import sys
 
 class SuffixNode:    
-    def __init__(self, index, rank):
+    def __init__(self, index, rank1, rank2):
         self.index = index
-        self.rank = rank
-        
+        self.rank = [rank1, rank2]
+           
 def buildSuffixArray(s):
     # Write your code here
     tlen = len(s)
     startIndex = ord("a")
-    suffixes  = [SuffixNode(i, [ord(s[i])-startIndex, ord(s[i+1])-startIndex if i+1 < tlen else -1]) for i in range(tlen)]
-    suffixes.sort(key = lambda x : x.rank) # suffix array is sorted 
+    suffixes  = [SuffixNode(i, ord(s[i])-startIndex, ord(s[i+1])-startIndex if i+1 < tlen else -1) for i in range(tlen)]
+    suffixes.sort(key = lambda x : (x.rank[0], x.rank[1])) # suffix array is sorted 
     suffixInd = [0] * tlen 
     k = 4
     while k <= 2*tlen:
@@ -23,7 +23,7 @@ def buildSuffixArray(s):
         suffixInd[suffixes[0].index] = 0
         for i in range(1,tlen):
             if (suffixes[i].rank[0] == prev_rank and
-                suffixes[i].rank[1] == suffixes[i-1].rank[0]):
+                suffixes[i].rank[1] == suffixes[i-1].rank[1]):
                 prev_rank = suffixes[i].rank[0]
                 suffixes[i].rank[0] = rank
             else:
@@ -34,8 +34,8 @@ def buildSuffixArray(s):
         for i in range(1, tlen):
             nextindex = suffixes[i].index + k//2
             suffixes[i].rank[1] = suffixes[suffixInd[nextindex]].rank[0] if nextindex < tlen else -1                        
-        suffixes.sort(key = lambda x:x.rank)
+        suffixes.sort(key = lambda x:(x.rank[0], x.rank[1]))
         k *= 2
     suffixA = [suffix.index for suffix in suffixes]    
-    print(suffixA)
-    return suffixA   
+    #print(suffixA)
+    return suffixA  
